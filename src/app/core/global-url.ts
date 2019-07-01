418b542a947bb65fe9423398189de7e845ea2726
Rami Al-Zebak <17zrami@gmail.com>
2019-07-01 12:30:44 +0300

    jenkins file

--- src/app/core/global-url.ts
+++ src/app/core/global-url.ts
@@ -1,5 +1,5 @@
 export class GlobalURL {
    // public static URL="http://localhost:3000/api/"
     
-     public static URL="http://localhost:3000/api/" 
+     public static URL="http://172.107.175.81:3000/api/" 
 }
