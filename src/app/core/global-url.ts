9424a178bcda5cca357db47b58cf85b1c786e873
Rami Al-Zebak <17zrami@gmail.com>
2019-09-07 17:24:23 +0300

    https api url

--- src/app/core/global-url.ts
+++ src/app/core/global-url.ts
@@ -1,5 +1,5 @@
 export class GlobalURL {
   //  public static URL="http://localhost:3000/api/"
     
-  public static URL="http://almersal.co/api/" 
+  public static URL="https://almersal.co/api/" 
 }
